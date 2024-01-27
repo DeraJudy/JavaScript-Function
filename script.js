7bdb1de3661e2296933b2d867c6d996112171617
DeraJudy <110675235+DeraJudy@users.noreply.github.com>
2024-01-26 22:28:17 -0800

    first upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// function sayHi() {
+//     console.log("Hi");
+//     console.log("Bye");
+// }
+// sayHi();
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// const value = sum(10, 100);
+// console.log(value);
+
+// function random(number) {
+//     const result = Math.floor(Math.random() * number);
+//     return result;
+// }
+  
+// Assignment from Odin Project
+function add7(a) {
+    return a + 7;
+}
+console.log(add7(9));
+
+
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(12, 89));
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+console.log(capitalize("chidera"));
+
+function lastLetter(inputString) {
+    return inputString.charAt(inputString.length - 1);
+}
+
+// Example usage:
+var inputStr = "abcd";
+var result = lastLetter(inputStr);
+console.log(result);
+
+console.log("hello world");
